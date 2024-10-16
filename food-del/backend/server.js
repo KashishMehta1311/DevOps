44cabadcd32d7bc85419cc96fe78f913181552f3
KashishMehta1311 <mehtakashish1311@gmail.com>
2024-10-16 18:03:31 +0530

    First commit, uploading files from laptop

---
+++ food-del/backend/server.js
@@ -0,0 +1,41 @@
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// Example of CORS configuration in a server
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+}));
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
